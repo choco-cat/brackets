e0ff650792b70ebee4d65de9750af11b56a3c480
Panfilenka Natallia <choco-cat@mail.ru>
2020-10-16 09:44:27 +0300

    feat: solution brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let bracketsConfigStr = [];
+    let oldStr = '';
+    bracketsConfig.forEach(elementArr => {
+        bracketsConfigStr.push(elementArr.join(''));
+    });
+
+    while (str !== oldStr) {
+        oldStr = str;
+        bracketsConfigStr.forEach(bracketsElement => {
+            str = str.replace(bracketsElement, '');
+        });
+    }
+
+    return str !== '' ? false : true;
 }
